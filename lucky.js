a4e94f8f2ed9fd3f53e71b4021c4be34a824098e
raineridev <116137268+raineridev@users.noreply.github.com>
2023-01-18 17:19:10 -0300

    Update lucky.js

--- lucky.js
+++ lucky.js
@@ -1,18 +1,18 @@
 const fs = require('fs');
-let jogadas = 1;
-let numeros = [];
+let moves = 1;
+let drawnNumbers = [];
 
 function lot(max) {
-    let ar = Math.random() * max
-    numeros.push(Math.floor(ar));
+    let repeatOperation = Math.random() * max
+    drawnNumbers.push(Math.floor(repeatOperation));
 }
-while (jogadas <= 6) {
-    jogadas++;
+while (moves <= 6) {
+    moves++;
     lot(60)
 }
-console.log(numeros)
+console.log(drawnNumbers)
 
 
-let texto = numeros.join(',');
+let texto = drawnNumbers.join(',');
 fs.writeFileSync('lucky-numbers.txt', texto);
 console.log('Text file generated successfully!');
